d7d51121fe0ce178e1cca42e635d91c985d4179e
confmatr37 <joannagebeau@gmail.com>
2026-08-03 09:14:50 -0500

    fix mismatched variable

--- script.js
+++ script.js
@@ -95,5 +95,5 @@ function myFunction() {
         var popup = document.getElementById("myPopup");
         popup.classList.toggle("show");
         const clue = atob(encodedClue);
-        document.getElementById("clue").textContent = clue;
+        document.getElementById("answer").textContent = clue;
 }
